4265125d06c16bb5394d4104f46f75d87d0dd156
elias.grosso <eliasactisgrosso@gmail.com>
2020-08-22 13:50:47 -0300

    first commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import TypoGraphy from '@material-ui/core/Typography';
+import Link from '@material-ui/core/Link';
+
+
+function NavBar(props) {
+
+    return (
+        <List component="nav">
+            <ListItem component="div">
+                <ListItemText inset>
+                    <TypoGraphy className="MuiTypography-colorError" color="inherit" variant="title" >
+                        <Link href="https://www.google.com.ar" color="inherit">Home</Link>
+                    </TypoGraphy>
+                </ListItemText>
+
+                <ListItemText inset>
+                    <TypoGraphy color="inherit" variant="title" >
+                    <Link href="https://www.google.com.ar" color="inherit">About</Link>
+                    </TypoGraphy>
+                </ListItemText>
+
+
+                <ListItemText inset>
+                    <TypoGraphy color="inherit" variant="title">
+                        <Link href="https://www.google.com.ar" color="inherit">Our work</Link>
+                    </TypoGraphy>
+                </ListItemText>
+
+
+                <ListItemText inset>
+                    <TypoGraphy color="inherit" variant="title">
+                        <Link href="https://www.google.com.ar" color="inherit">Find us</Link>
+                    </TypoGraphy>
+                </ListItemText>
+            </ListItem >
+        </List>
+    )
+}
+
+
+export default NavBar;
+
